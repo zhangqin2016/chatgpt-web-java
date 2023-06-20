8bde895e8f6e0f45865d1767ae297ecdbf84783f
zhangqin <492297036@qq.com>
2023-06-20 18:43:59 +0800

    1

---
+++ web/app/src/components/Setting/About.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Author: mjjh
+ * @LastEditTime: 2023-04-16 20:12:48
+ * @FilePath: \chagpt-shuowen\src\components\Setting\About.vue
+ * @Description:
+-->
+<script setup lang='ts'>
+</script>
+
+<template>
+  <div class="p-4 space-y-4">
+
+  </div>
+</template>
